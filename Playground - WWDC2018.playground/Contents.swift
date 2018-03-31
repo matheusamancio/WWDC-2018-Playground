8e25aff9b635434729a19197b3aa436480743d51
Matheus Amancio <matheusamancio23@hotmail.com>
2018-03-31 16:51:51 -0300

    put song on the project

--- Playground - WWDC2018.playground/Contents.swift
+++ Playground - WWDC2018.playground/Contents.swift
@@ -1,11 +1,34 @@
 //#-hidden-code
-
+import AVFoundation
 import UIKit
 import PlaygroundSupport
+
 let viewteste = UIView(frame: CGRect(x: 0, y: 0, width: 700, height: 400))
 var controller: Controller?
 controller = Controller(view:viewteste)
 viewteste.center
+
+
+
+var backgroundMusicPlayer = AVAudioPlayer()
+
+var player: AVAudioPlayer?
+func playSound() {
+    let url = Bundle.main.url(forResource: "PianoSolo", withExtension: "mp3")!
+    
+    do {
+        player = try AVAudioPlayer(contentsOf: url)
+        guard let player = player else { return }
+        player.prepareToPlay()
+        player.play()
+        
+    } catch let error {
+        print(error.localizedDescription)
+    }
+}
+playSound()
+
+
 //class MyViewController : UIViewController {
 //    override func loadView() {
 //        mylabel.frame = CGRect(x: 30, y: 30, width: 400, height: 150)
--- Playground - WWDC2018.playground/Sources/Scene.swift
+++ Playground - WWDC2018.playground/Sources/Scene.swift
@@ -97,9 +97,8 @@ public class Scenes{
             self.fadeOutInLabel(text:text, textColor: textColor, delay: delay)
             let id = i + 1
             event(i: i)
-//            DispatchQueue.main.asyncAfter(deadline: .now() + 4 + delay) {
-            DispatchQueue.main.asyncAfter(deadline: .now()) {
-
+            DispatchQueue.main.asyncAfter(deadline: .now() + 4 + delay) {
+//            DispatchQueue.main.asyncAfter(deadline: .now()) {
                 self.sceneSequency(i: id)
             }
         }
