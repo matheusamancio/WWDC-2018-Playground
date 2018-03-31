10e8b519267416ecf7643c664441fb8cea9eb0e4
Matheus Amancio <matheusamancio23@hotmail.com>
2018-03-31 11:14:28 -0300

    design the Intro Screen

--- Playground - WWDC2018.playground/Sources/Intro.swift
+++ Playground - WWDC2018.playground/Sources/Intro.swift
@@ -3,7 +3,9 @@ import UIKit
 public class Intro{
     
     private var view: UIView
-    private var label: UILabel
+    private var labelSubTitle: UILabel
+    private var labelTitle: UILabel
+    private var labelExplanation: UILabel
     private var startButton: UIButton
     public var goToScenes: (()->())?
     private var clicked: Int{
@@ -17,7 +19,9 @@ public class Intro{
     public init(){
         self.clicked = 0
         self.view = UIView(frame: CGRect(x: 0, y: 0, width: 700, height: 400))
-        self.label = UILabel()
+        self.labelSubTitle = UILabel()
+        self.labelTitle = UILabel()
+        self.labelExplanation = UILabel()
         self.startButton = UIButton()
 
     }
@@ -27,21 +31,46 @@ public class Intro{
     }
     private func buildIntro(){
         // center
+        self.view.backgroundColor = UIColor.white
         let centerWidth = view.frame.width/2
         let centerHeight = view.frame.height/2
         
-        //label
-        self.label.frame = CGRect(x: 30, y: 30, width: 400, height: 150)
-        self.label.text = "teste"
-        self.label.tintColor = UIColor.black
-        self.view.backgroundColor = UIColor.white
-        self.view.addSubview(self.label)
+        //label subtitle
+        self.labelSubTitle.frame = CGRect(x: 30, y: 10, width: 100, height: 50)
+        self.labelSubTitle.text = "Hi, I’m earth"
+        self.labelTitle.font = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.thin)
+        self.labelSubTitle.tintColor = UIColor.black
+        self.view.addSubview(self.labelSubTitle)
+        
+        //label Title
+        self.labelTitle.frame = CGRect(x: 30, y: 60, width: 400, height: 50)
+        self.labelTitle.text = "Welcome to my Playground"
+        self.labelTitle.font = UIFont.systemFont(ofSize: 30, weight: UIFont.Weight.bold)
+        self.labelTitle.tintColor = UIColor.black
+        self.view.addSubview(self.labelTitle)
+
+        //label Explanation
+        self.labelExplanation.frame = CGRect(x: 30, y: 80, width: 400, height: 150)
+        self.labelExplanation.numberOfLines = 3
+        self.labelExplanation.text = "This is a short scene, that you will listen what \nthe earth has to speak. \nSo, prepare your heart to listen and help her."
+
+        self.labelExplanation.font = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.thin)
+        self.labelExplanation.tintColor = UIColor.black
+        self.view.addSubview(self.labelExplanation)
         
         //button
-        self.startButton.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
-        self.startButton.setTitle("Watch now", for: .normal)
+        self.startButton.frame = CGRect(x: 30, y: 220, width: 180, height: 40)
         self.startButton.backgroundColor = UIColor.black
+        self.startButton.setTitle("  Watch now", for: .normal)
+        self.startButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.medium)
+        self.startButton.contentHorizontalAlignment = .left
+        self.startButton.layer.cornerRadius = 8
         self.startButton.addTarget(self, action: #selector(StartAnimation), for: .touchUpInside)
+        let imageView1 = UIImageView()
+        imageView1.image = #imageLiteral(resourceName: "iArrow.png")
+        imageView1.frame = CGRect(x: 115, y: 12.5, width: 56, height: 15)
+        self.startButton.layer.masksToBounds = true
+        self.startButton.addSubview(imageView1)
         self.view.addSubview(startButton)
     }
     
